a8961f0a2f608309c658853a5241e38d9d260d5b
rezwansaki <rezwansaki@gmail.com>
2020-12-21 04:52:45 +0600

    solve toggle menu problem

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,22 @@
-$(document).ready(function () {
-    $(".nav-toggler").each(function (_, navToggler) {
-        var target = $(navToggler).data("target");
-        $(navToggler).on("click", function () {
-            $(target).animate({
-                width: ["toggle", "swing"],
-                height: ["toggle", "swing"],
-                opacity: "toggle"
-            }, {
-                duration: 500,
-                specialEasing: {
-                    width: "linear",
-                    height: "easeOutBounce"
-                }
-            });
-        });
-    })
-})
+// $(document).ready(function () {
+//     $(".nav-toggler").each(function (_, navToggler) {
+//         var target = $(navToggler).data("target");
+//         $(navToggler).on("click", function () {
+//             $(target).animate({
+//                 width: ["toggle", "swing"],
+//                 height: ["toggle", "swing"],
+//                 opacity: "toggle"
+//             }, {
+//                 duration: 500,
+//                 specialEasing: {
+//                     width: "linear",
+//                     height: "easeOutBounce"
+//                 }
+//             });
+//         });
+//     })
+// })
+
+document.getElementById('nav-toggler').onclick = function () {
+    document.getElementById("navigation").classList.toggle("hidden");
+}
